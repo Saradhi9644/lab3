37685636b9b5fb9deebd5dbd44142bcca19ac1b9
Saradhi9644 <79004277+Saradhi9644@users.noreply.github.com>
2021-02-14 19:51:30 +0530

    Add files via upload

---
+++ Ex8.java
@@ -0,0 +1,17 @@
+package COM;
+
+
+import java.util.Scanner;
+public class Ex8 {
+public static void main(String[]args)
+{
+	Scanner scan=new Scanner(System.in);
+	String Str=scan.nextLine();
+	if(Str.charAt(0)<=Str.charAt(1)&&Str.charAt(1)<=Str.charAt(2)&&
+		Str.charAt(2) <= Str.charAt(3) && Str.charAt(3) <=Str.charAt(4)) {
+		System.out.println("Positive String");
+	}
+	else
+		System.out.println("Negative String");
+}
+}
